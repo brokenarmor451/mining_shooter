5483c61aa90d171acbaf2e8822f1e17d40a85958
Harun Uzunkaya <h94599771@gmail.com>
2026-01-29 20:10:22 +0300

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,11 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-<<<<<<< HEAD
         applicationId = "io.github.mining_shooter"
-=======
-        applicationId = "com.example.mining_shooter"
->>>>>>> origin/main
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
